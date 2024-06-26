dfe47be97965d0eef4daf82fe8c0921f5f950160
Léa Bog <61688722+LB0g@users.noreply.github.com>
2024-06-26 11:35:55 +0200

    Create Install_R_packages.R

---
+++ Install_R_packages.R
@@ -0,0 +1,15 @@
+## TO RUN IN THE RSTUDIO CONSOLE
+## THIS WILL CHECK IF THE PACKAGES ARE ALREADY INSTALLED AND INSTALL THE ONES THAT ARE NEEDED ONLY.
+
+# Define the packages you want to install
+packages <- c("data.table", "rlist", "hrbrthemes", "viridis", "tidyverse", "shiny", "plotly", "magick")
+# Find out which packages are already installed
+installed_packages <- rownames(installed.packages())
+# Determine which packages are not installed
+packages_to_install <- setdiff(packages, installed_packages)
+# Install the packages that are not yet installed
+if (length(packages_to_install) > 0) {
+  install.packages(packages_to_install)
+} else {
+  cat("All packages are already installed.\n")
+}
